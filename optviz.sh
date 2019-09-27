ad389e958b0c84009215b3b2893f61e7c2127760
Tanel Poder <tanel@tanelpoder.com>
2019-09-27 11:15:08 +0200

    better ordering for flamegraph

--- optviz.sh
+++ optviz.sh
@@ -2,7 +2,7 @@
 
 FNAME=`basename $1`
 
-./cbosummary.py $1 | grep -v '^$' | awk -F'|' '{ print $6 }' | sed 's/^ //' | grep -v '^$' | \
+./cbosummary.py $1 | grep -v '^$' | awk -F'|' '{ print $4 " " $6 }' | grep -v '^ *$' | awk '{ print $2 ";" $4 " " $1 }' | \
   flamegraph.pl --countname=Cost --title=OptViz-$FNAME - > $FNAME.svg
 
 open $FNAME.svg
